6913002d21fda987de0ceb31fb75aca4b55fb335
Ushakov <just_lad@mail.ru>
2023-10-17 21:52:36 +0300

    new receiver

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/', '/dist/', 'tests/Task2.spec.ts', 'tests/Task3.spec.ts', 'tests/Task4.spec.ts', 'tests/Task5.spec.ts'],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/', 'tests/Task2.spec.ts', 'tests/Task3.spec.ts', 'tests/Task4.spec.ts', 'tests/Task1.spec.ts'],
 };
 
 export default config;
